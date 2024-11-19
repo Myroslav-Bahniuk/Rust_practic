9844d988c34b0c6ef19c61cc1a569f148e464b67
Myroslav-Bahniuk <m.bagnyuk@e-u.edu.ua>
2024-11-19 19:10:22 +0200

    Update pr_6.rs

--- pr_6.rs
+++ pr_6.rs
@@ -1,24 +1,14 @@
-const WIDTH: usize = 30; // Ширина конверта
-const HEIGHT: usize = 15; // Высота конверта
+fn gcd(a: u32, b: u32) -> u32 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
 
 fn main() {
-    // Проходим по всем строкам
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            if y == 0 || y == HEIGHT - 1 {                
-                print!("*");
-            } 
-            else if x == 0 || x == WIDTH - 1 {        
-                print!("*");
-            } 
-            else if x == y || x == WIDTH - y - 1 {               
-                print!("*");
-            } 
-            else {
-                print!(" ");
-            }
-        }
-        // Переход на новую строку после каждой строки
-        println!();
-    }
+    let num1 = 56;
+    let num2 = 98;
+
+    println!("The GCD of {} and {} is {}", num1, num2, gcd(num1, num2));
 }
