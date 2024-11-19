698bea94936cf9d044ee29fe54fdbef64dd81b23
Myroslav-Bahniuk <m.bagnyuk@e-u.edu.ua>
2024-11-19 19:09:21 +0200

    Update pr_5.rs

--- pr_5.rs
+++ pr_5.rs
@@ -1,26 +1,24 @@
-const WIDTH: usize = 14; // Ширина конверта (необхідно для правильного відображення)
-const HEIGHT: usize = 7; // Висота конверта
+const WIDTH: usize = 30; // Ширина конверта
+const HEIGHT: usize = 15; // Высота конверта
 
 fn main() {
-    
-    for i in 0..HEIGHT {
-        
-        print!("{}", "*");
-
-       
-        print!("{}", " ".repeat(2 * i - 1));
-
-       
-        if i != 0 {
-            print!("{}", "*");
+    // Проходим по всем строкам
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            if y == 0 || y == HEIGHT - 1 {                
+                print!("*");
+            } 
+            else if x == 0 || x == WIDTH - 1 {        
+                print!("*");
+            } 
+            else if x == y || x == WIDTH - y - 1 {               
+                print!("*");
+            } 
+            else {
+                print!(" ");
+            }
         }
-
-        
+        // Переход на новую строку после каждой строки
         println!();
-    }
-
-    
-    for _ in 0..WIDTH {
-        println!("{}", "*".repeat(WIDTH * 2 - 1)); // Виводимо широку лінію
     }
 }
